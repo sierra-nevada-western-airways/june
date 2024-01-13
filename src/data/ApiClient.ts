b0c0a1feb7ff9f5cc8901f03ab73fc332d254da7
Michael B <mjbradvica@gmail.com>
2024-01-13 13:36:49 -0500

    config for swagger

---
+++ src/data/ApiClient.ts
@@ -0,0 +1,10 @@
+import type { AxiosRequestConfig } from "axios";
+import { Api } from "./Api";
+
+export default function apiClient(): Api<void> {
+  const config: AxiosRequestConfig = {
+    baseURL: process.env.VITE_API_URL as string,
+  };
+
+  return new Api(config);
+}
